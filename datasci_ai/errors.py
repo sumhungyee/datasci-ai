1f0cfdc56633bfa1f7089394057e62aace7245cb
HY <e0726431@u.nus.edu>
2023-06-24 14:28:28 +0800

    fix typos in code

--- datasci_ai/errors.py
+++ datasci_ai/errors.py
@@ -1,14 +1,14 @@
 class DataSciAIError(Exception):
     def __init__(self, message="datasci_ai encountered an error!"):
         self.message = message
-        super.__init__(self.message)
+        super().__init__(self.message)
 
 class LanguageError(DataSciAIError):
     def __init__(self, language):
         self.message = f"Expected python, got {language}!"
-        super.__init__(self.message)
+        super().__init__(self.message)
 
 class CodeDetectionError(DataSciAIError):
     def __init__(self):
         self.message = "No code blocks detected! Cannot execute code!"
-        super.__init__(self.message)
+        super().__init__(self.message)
