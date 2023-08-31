bb3085e7d3cd75953e2f7990d499491373995dc9
HY <e0726431@u.nus.edu>
2023-09-01 00:40:09 +0800

    add ctransformers

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='datasci_ai',
-    version='1.0.0',
+    version='1.1.0',
     author='Sum Hung Yee',
     author_email='hungyee2013@email.com',
     url='https://https://github.com/sumhungyee/datasci-ai/',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 2 - Planning',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.10',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.10',
     install_requires=[
+        'ctransformers',
         'transformers',
         'pandas',
         'numpy',
